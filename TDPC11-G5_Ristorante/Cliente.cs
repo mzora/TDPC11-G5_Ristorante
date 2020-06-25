d108e630453e1c8af6c9ca03b837dbcce6a981c1
Carmelo Tumino <65621743+ctumino@users.noreply.github.com>
2020-06-25 15:07:22 +0200

    Update Cliente.cs

--- TDPC11-G5_Ristorante/Cliente.cs
+++ TDPC11-G5_Ristorante/Cliente.cs
@@ -7,6 +7,7 @@ namespace TDPC11_G5_Ristorante
 {
     public class Cliente
     {
+        public int IDN { get; set;}
         public string Username { get; set; }
         public string Password { get; set; }
         public string Cognome { get; set; }
